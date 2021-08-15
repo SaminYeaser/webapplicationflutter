20ad1a8fa82b7766afd9715902fd3f5846561038
SaminYeaser <saminyeaser1@gmail.com>
2021-08-16 05:14:37 +0600

    overview page for medium screen made

--- lib/pages/overview/widgets/overviewCardMedium.dart
+++ lib/pages/overview/widgets/overviewCardMedium.dart
@@ -1,14 +1,54 @@
 import 'package:flutter/material.dart';
 
+import 'infoCard.dart';
+
 class OverViewCardMedium extends StatelessWidget {
   const OverViewCardMedium({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    double _width = MediaQuery.of(context).size.width;
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-
+        Row(
+          children: [
+            InfoCard(
+              title: 'Rides in Progress',
+              value: '7',
+              onTap: (){},
+              topColor: Colors.orange,
+            ),
+            SizedBox(
+              width: _width/64,
+            ),
+            InfoCard(
+              title: 'Packages Delivered',
+              value: '17',
+              onTap: (){},
+              topColor: Colors.lightGreen,
+            ),
+            Row(
+              children: [
+                InfoCard(
+                  title: 'Schedule Delivers',
+                  value: '5',
+                  onTap: (){},
+                  topColor: Colors.orangeAccent,
+                ),
+                SizedBox(
+                  width: _width/64,
+                ),
+                InfoCard(
+                  title: 'Cancel Orders',
+                  value: '3',
+                  onTap: (){},
+                  topColor: Colors.redAccent,
+                ),
+              ],
+            )
+          ],
+        )
       ],
     );
   }
